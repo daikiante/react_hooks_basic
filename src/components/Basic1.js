131bd1453460fa21ae71efab0cb9c212d54e1573
daikiante <daikiante@gmail.com>
2020-09-23 13:02:01 +0900

    start Hooks

--- src/components/Basic1.js
+++ src/components/Basic1.js
@@ -4,20 +4,30 @@ import React, {useState} from 'react'
 
 // Hello React を返す関数
 // 引数は props と記述する(決まり事)
-const Basic1 = (props) => {
-    const clickHundler = () => {
-        console.log('クリックされました')
-    }
+const Basic1 = () => {
+    
 
-    // カウンターを定義
-    const [count, setCount] = useState(0)
+    // プロダクト関数を定義
+    // 関数名 : setProducts  オブジェクト : product
+    const [product, setProducts] = useState({name: '', price: ''})
     return (
+        // 入力された値を動的に管理する
+        // onChange : 値が変わった時呼び出される
+        // 値が変わった時、setProductsで変更した内容をevtに保存する
+        // evt.target.value : 現在入力されているフォームの値を取得している
+        // ... : productの要素を分解してくれる(付けないと、nameを書き換えた時にpriceが初期値になってしまう)
+
         // div もしくは <></> で囲っておく制約がある
         <>
-            <button onClick={clickHundler}>Click</button>
-            <button onClick={() => setCount(count + 1)}>Count {count}</button>
-            <h1>Hello { props.name }</h1>
-            <h1>Hello React 2</h1>
+            <form>
+                <input type='text' value={product.name}onChange={evt => setProducts({...product, name: evt.target.value})}/>
+
+                <input type='text' value={product.price}onChange={evt => setProducts({...product, price: evt.target.value})}/>
+
+            </form>
+
+            <h3>Product name is {product.name}</h3>
+            <h3>Product price is {product.price}</h3>
         </>
     )
 }
